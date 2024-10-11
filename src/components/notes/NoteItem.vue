7d19c5cdf2f7cca9dafc0b4688b2003dcfa85d56
pcruz <pablo.cruz2497@gmail.com>
2024-10-11 11:41:22 -0300

    componente note item

---
+++ src/components/notes/NoteItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+//recibira nota via props
+const { note } = defineProps({
+  note: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+<template>
+  <div class="card h-100">
+    <div class="card-header">
+      <h3>{{ note.title }}</h3>
+    </div>
+    <div class="card-body">
+      <p class="card-text">{{ note.content }}</p>
+    </div>
+  </div>
+</template>
